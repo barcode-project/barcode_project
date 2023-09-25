5b6ba0b5a0c5b4956b22c96eed37308f20d45fdc
RAMI <ygyemengas@gmail.com>
2023-09-25 17:04:32 +0300

    frist 1.6

---
+++ app/src/main/java/com/example/barcode/splash_screen.java
@@ -0,0 +1,64 @@
+package com.example.barcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences userPref = getApplicationContext().getSharedPreferences("user", MODE_PRIVATE);
+                boolean isLoggedIn = userPref.getBoolean("isLoggedIn", false);
+                boolean isPlaced = userPref.getBoolean("isPlaced", false);
+                String type = userPref.getString("type", "non");
+                if (isLoggedIn) {
+                    switch (type) {
+                        case "user":
+                            startActivity(new Intent(splash_screen.this, MainActivity.class));
+                            finish();
+                        default:
+                            // Do something when no option is selected
+                            break;
+                    }
+
+                } else {
+                    isFirstTime();
+                }
+            }
+        }, 3000);
+
+    }
+
+    private void isFirstTime() {
+        //for checking if the app is running for the very first time
+        //we need to save a value to shared preferences
+        SharedPreferences preferences = getApplication().getSharedPreferences("onBoard", MODE_PRIVATE);
+        boolean isFirstTime = preferences.getBoolean("isFirstTime", true);
+        //default value true
+        if (isFirstTime) {
+            // if its true then its first time and we will change it false
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("isFirstTime", false);
+            editor.apply();
+
+            // start main activity
+            startActivity(new Intent(splash_screen.this, login_page.class));
+            finish();
+        } else {
+            //start login Activity
+            startActivity(new Intent(splash_screen.this, MainActivity.class));
+            finish();
+        }
+    }
+}
