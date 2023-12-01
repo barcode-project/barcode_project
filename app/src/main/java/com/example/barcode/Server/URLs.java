75680193e14eccd97efe51860739b4d8f8133912
GamalGamil66 <135033603+GamalGamil66@users.noreply.github.com>
2023-12-01 22:47:55 +0300

    api

--- app/src/main/java/com/example/barcode/Server/URLs.java
+++ app/src/main/java/com/example/barcode/Server/URLs.java
@@ -4,8 +4,8 @@ public class URLs {
 //    public static final String URL = "http://192.168.43.128:8080/";
     public static final String URL = "https://demo.qryemen.com/";
     public static final String HOME = URL+"api/";
-    public static final String Login = HOME+"login";
-    public static final String Get_Streets = HOME+"get_streets";
-    public static final String Get_Orgs = HOME+"get_orgs";
-    public static final String Get_Org = HOME+"get_org";
+    public static final String Login = HOME+"worker/login";
+    public static final String Get_Streets = HOME+"worker/get_streets";
+    public static final String Get_Orgs = HOME+"worker/get_orgs";
+    public static final String Get_Org = HOME+"worker/get_org";
 }
