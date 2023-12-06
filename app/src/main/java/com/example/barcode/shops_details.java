781a886addbfac13b32358771fd2438ea3dc94e2
ALMOSAWA <ahmedalmosawawww2000@gmail.com>
2023-12-06 18:11:24 +0300

    ahmed16

--- app/src/main/java/com/example/barcode/shops_details.java
+++ app/src/main/java/com/example/barcode/shops_details.java
@@ -1,8 +1,6 @@
 package com.example.barcode;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
@@ -10,10 +8,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,7 +17,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.widget.ContentLoadingProgressBar;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -38,7 +33,6 @@ import com.example.barcode.utils.Tools;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputEditText;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -51,23 +45,24 @@ public class shops_details extends AppCompatActivity {
 
     private TemplatePDF templatePDF;
     private ImageView bt_exit;
-    TextInputEditText id_no,last_licens,owner_name,shop_name,phone_no,activity_type,neighbor_unit,address_unit;
-    TextView front_signboard,side_signboard,elec_signboard,supetficail_signboard,stuck_signboard,mural_signborard,totl_price;
+    TextInputEditText id_no, last_licens, owner_name, shop_name, phone_no, activity_type, neighbor_unit, address_unit;
+    TextView front_signboard, side_signboard, elec_signboard, supetficail_signboard, stuck_signboard, mural_signborard, totl_price;
     Button btnPdfReceipt, btnThermalPrinter;
     AppCompatButton show_billboard;
     ContentLoadingProgressBar progressBar;
     private SharedPreferences sharedPreferences;
 
-     String currency="", shopname="احمد", shop_contact, shop_email, shop_address=String.valueOf("الرويشان") , shortText;
+    String currency="R" ,activitytype, shopname, shop_contact, ownername, shop_address = String.valueOf("الرويشان"), shortText;
 
-     String insitiution_number="08967674490",order_time,order_date="12/12/2000";
+    String insitiution_number = "08967674490", order_time, order_date = "12/12/2000";
     //how many headers or column you need, add here by using ,
     //headers and get clients para meter must be equal
     private String[] header = {"المبلغ", "الرسوم"};
-    private  String[] shortTex = {null},longText;
+    private String[] shortTex = {null}, longText;
     Bitmap bm = null;
     private static final int REQUEST_CONNECT = 100;
     private int id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -82,13 +77,6 @@ public class shops_details extends AppCompatActivity {
         activity_type=findViewById(R.id.activity_type);
         neighbor_unit=findViewById(R.id.neighbor_unit);
         address_unit=findViewById(R.id.address_unit);
-//        front_signboard=findViewById(R.id.front_signboard);
-//        side_signboard=findViewById(R.id.side_signboard);
-//        elec_signboard=findViewById(R.id.elec_signboard);
-//        supetficail_signboard=findViewById(R.id.supetficail_signboard);
-//        stuck_signboard=findViewById(R.id.stuck_signboard);
-//        mural_signborard=findViewById(R.id.mural_signborard);
-//        totl_price=findViewById(R.id.totl_price);
         show_billboard = findViewById(R.id.show_billboard);
         btnThermalPrinter=findViewById(R.id.printBtn);
         btnPdfReceipt=findViewById(R.id.save_pdf_Btn);
@@ -101,23 +89,6 @@ public class shops_details extends AppCompatActivity {
         shortText = "Customer Name: Mr/Mrs. " + "customer_name";
 
         longText = new String[]{"نشكركم"};
-
-        ArrayList<PlateData> plateList = new ArrayList<>();
-//        plateList.add(new PlateData("نوع اللوحة 1", 5, 100.0, 50.0));
-        plateList.add(new PlateData("نوع اللوحة 1", 5, 100.0, 50.0));
-//        plateList.add(new PlateData("نوع اللوحة 2", 5, 100.0, 50.0));
-        plateList.add(new PlateData("نوع اللوحة 2", 3, 80.0, 40.0));
-        // ... أضف المزيد من البيانات إذا لزم الأمر
-//        String[] items={"5, 100.0, 50.0","411","412","413","415"};
-        String[] items={"5, 100.0, 50.0","411","412","413","415"};
-        // قم بربط بيانات اللوحة بـ ArrayAdapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-
-//     ArrayAdapter<PlateData> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, items);
-//
-        // ربط القائمة بـ ListView
-//        ListView listView = findViewById(R.id.listView);
-//        listView.setAdapter(adapter);
 
 
 //            try {
@@ -200,8 +171,8 @@ public class shops_details extends AppCompatActivity {
 
     private ArrayList<String[]> getOrdersData() {
         ArrayList<String[]> rows = new ArrayList<>();
-        rows.add(new String[]{currency + shopname,"رسوم محل"});
-        rows.add(new String[]{currency + "40000","دعاية وإعلان"});
+        rows.add(new String[]{currency + "shopname", "رسوم محل"});
+        rows.add(new String[]{currency + "40000", "دعاية وإعلان"});
         rows.add(new String[]{currency + "2000","نظافة"});
         rows.add(new String[]{currency + "1200","نظافة مهن"});
         rows.add(new String[]{currency + "2500","رسوم الخدمة"});
@@ -212,14 +183,14 @@ public class shops_details extends AppCompatActivity {
     }
     private ArrayList<String[]> gethaderData() {
         ArrayList<String[]> rows = new ArrayList<>();
-        rows.add(new String[]{ shopname});
+//        rows.add(new String[]{ shopname});
         rows.add(new String[]{"دائرة الخدمات الالكترونية"});
         rows.add(new String[]{"مديرية الوحدة"});
         rows.add(new String[]{"نظافة مهن"});
         rows.add(new String[]{"الوقت والتاريخ"});
-        rows.add(new String[]{"نوع النشاط"});
-        rows.add(new String[]{"اسم المنشأه"});
-        rows.add(new String[]{"اسم المالك"});
+        rows.add(new String[]{" نوع النشاط: "+activitytype});
+        rows.add(new String[]{"اسم المنشأه: "+shopname});
+        rows.add(new String[]{"اسم المالك: "+ownername});
         rows.add(new String[]{"رقم التواصل"});
 
         return rows;
@@ -235,14 +206,19 @@ public class shops_details extends AppCompatActivity {
                         JSONObject object = new JSONObject(response);
                         if (object.getBoolean("success")) {
                             JSONObject citizen = new JSONObject(object.getString("data"));
-                                owner_name.setText(citizen.getString("owner_name"));
-                                id_no.setText(String.valueOf(citizen.getInt("id")));
-                                shop_name.setText(citizen.getString("org_name"));
-                                phone_no.setText(citizen.getString("owner_phone"));
-                                address_unit.setText(citizen.getString("street_name"));
-                                activity_type.setText(citizen.getString("org_type_name"));
+                            owner_name.setText(citizen.getString("owner_name"));
+                            id_no.setText(String.valueOf(citizen.getInt("id")));
+                            shop_name.setText(citizen.getString("org_name"));
+                            phone_no.setText(citizen.getString("owner_phone"));
+                            address_unit.setText(citizen.getString("street_name"));
+                            activity_type.setText(citizen.getString("org_type_name"));
 
                         }
+                        shopname = String.valueOf(shop_name.getText());
+                        ownername = String.valueOf(owner_name.getText());
+                        activitytype = String.valueOf(activity_type.getText());
+
+
                     } catch (JSONException e) {
                         e.printStackTrace();
                         Toast.makeText(shops_details.this, e.getMessage(), Toast.LENGTH_SHORT).show();
